6f01cc80d53c77df02f466741f632b44bb733be3
zhujun98 <zhujun981661@gmail.com>
2021-06-22 22:26:36 +0200

    Update Spark Java app

---
+++ dev_envs/spark_docker/spark-submit/java-app/src/main/java/Hello.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class Hello {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder().appName("spark_test").getOrCreate();
+
+        List<String> logOfSongs = Arrays.asList(
+            "Despacito",
+            "Nice for what",
+            "No tears left to cry",
+            "Despacito",
+            "Havana",
+            "In my feelings",
+            "Nice for what",
+            "despacito",
+            "All the stars"
+        );
+
+        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
+        JavaRDD<String> distributedSongLog = sc.parallelize(logOfSongs);
+        System.out.println(distributedSongLog.map((x) -> { return x.toLowerCase(); }).collect());
+
+        spark.stop();
+    }
+}
