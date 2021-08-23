e0e5f36e8d530882ecb7e8a235302fd5e0d6e262
zhujun98 <zhujun981661@gmail.com>
2021-08-23 21:20:07 +0200

    Add config.ini file for cluster configuration

--- opt_pub_trans_with_kafka/producers/models/producer.py
+++ opt_pub_trans_with_kafka/producers/models/producer.py
@@ -1,14 +1,21 @@
 """Producer base-class providing common utilites and functionality"""
+import configparser
 import logging
+from pathlib import Path
 import socket
 import time
-
 
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read(Path(__file__).parents[0].joinpath('..', 'config.ini'))
+
+BROKER_URL = config['CLUSTER']['BROKER_URL']
+SCHEMA_REGISTRY_URL = config['CLUSTER']['SCHEMA_REGISTRY_URL']
 
 
 class Producer:
@@ -21,42 +28,41 @@ class Producer:
                  value_schema=None,
                  num_partitions=1,
                  num_replicas=1):
-        self.topic_name = topic_name
-        self.key_schema = key_schema
-        self.value_schema = value_schema
-        self.num_partitions = num_partitions
-        self.num_replicas = num_replicas
+        self._topic_name = topic_name
+        self._key_schema = key_schema
+        self._value_schema = value_schema
+        self._num_partitions = num_partitions
+        self._num_replicas = num_replicas
 
-        self.broker_url = "PLAINTEXT://localhost:9092"
         self.broker_properties = {
-            "bootstrap.servers": self.broker_url,
+            "bootstrap.servers": BROKER_URL,
             "client.id": socket.gethostname()
             # TODO
         }
 
         # If the topic does not already exist, try to create it
-        if self.topic_name not in self.existing_topics:
+        if self._topic_name not in self.existing_topics:
             self.create_topic()
-            Producer.existing_topics.add(self.topic_name)
+            Producer.existing_topics.add(self._topic_name)
 
         self.producer = AvroProducer(
-            self.broker_properties, schema_registry="http://localhost:8081"
+            self.broker_properties, schema_registry=SCHEMA_REGISTRY_URL
         )
 
     def create_topic(self):
         """Creates the producer topic."""
-        client = AdminClient({"bootstrap.servers": self.broker_url})
+        client = AdminClient({"bootstrap.servers": BROKER_URL})
         # TODO: maybe add config for NewTopic
         future = client.create_topics([
-            NewTopic(topic=self.topic_name,
-                     num_partitions=self.num_partitions,
-                     replication_factor=self.num_replicas)
+            NewTopic(topic=self._topic_name,
+                     num_partitions=self._num_partitions,
+                     replication_factor=self._num_replicas)
         ])
         try:
             future.result()
-            logger.info(f"New topic created: {self.topic_name}")
+            logger.info(f"New topic created: {self._topic_name}")
         except Exception as e:
-            logger.error(f"Failed to create topic: {self.topic_name}")
+            logger.error(f"Failed to create topic: {self._topic_name}")
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
